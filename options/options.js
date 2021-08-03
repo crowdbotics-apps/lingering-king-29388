98ea67db40f4c4837f01e0edaa855365f2927e14
Crowdbotics <team@crowdbotics.com>
2021-08-03 11:21:35 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "lingering_king_29388Identifier",
+  url: "https://lingering_king_29388Identifier.botics.co",
+  api: "https://lingering_king_29388Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
